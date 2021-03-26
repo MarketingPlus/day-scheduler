7ad196ff1a67d8522137ac4f27ac2fe396696f95
MarketingPlus <benmarketingplus@gmail.com>
2021-03-27 08:07:17 +1100

    added small a small function which will manage changing the colour depending on the hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,19 @@
+var saveBtn = $(".saveBtn");
+
+// =============================== MOMENT JS ===============================
+
+// Displays the time ontop of the webpage
 var currentDay = moment().format("lll");
 $('#currentDay').text(currentDay);
+
+
+// ==================== TEXTAREA (PAST, PRESENT, FUTURE) ==================
+
+// function to change the colour depending on the time
+function colorChange() {
+    var hour = moment().hours();
+
+    $(".time").each(function() {
+    
+    })
+};
