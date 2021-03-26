5a97f22fe24c21506c00ce12edf442fbd757bb17
MarketingPlus <benmarketingplus@gmail.com>
2021-03-27 08:15:03 +1100

    created a function that adds the premade classes whether if the time is less equal or greater than the time on the side

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,18 @@ function colorChange() {
     var hour = moment().hours();
 
     $(".time").each(function() {
-    
+        var currentHour = parseInt($(this).attr("id"));
+
+        // console.log(this)
+
+        if (currentHour > hour) {
+            $(this).addClass("future");
+        } else if (currentHour === hour) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("past");
+        }
     })
 };
+
+colorChange()
